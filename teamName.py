b52614a4fb9af765775af99b3ab21f67ad9ade10
Nafi Robayat <nafi@live.com>
2022-07-09 04:08:05 +1000

    Added autocorrelation graphs

--- teamName.py
+++ teamName.py
@@ -1,7 +1,16 @@
 import numpy as np
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf
 
 nInst=100
 currentPos = np.zeros(nInst)
+
+all_instruments = [i for i in range(nInst)]
+df = pd.read_csv("./prices.txt", delim_whitespace=True, names=all_instruments)
+arr = df[all_instruments]
+
+plot_acf(arr[0], lags=40)
 
 def getMyPosition (prcSoFar):
     global currentPos
@@ -9,5 +18,3 @@ def getMyPosition (prcSoFar):
     # Build your function body here
 
     return currentPos
-
-    
